31127254e2513020bf7d4d0c7738033f68760fc5
zelimir <zelimir.ilic@consolia.rs>
2022-11-08 14:13:48 +0100

    PromisesProject

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -1,9 +1,16 @@
 import setText, {appendText, showWaiting, hideWaiting} from "./results.mjs";
 
 export function get() {
+    axios.get("http://localhost:3000/orders/1")
+    .then(({data}) => {
+        setText(JSON.stringify(data));
+    })
 }
 
 export function getCatch() {
+    axios.get("http://localhost:3000/orders/123").then(({data}) => {
+        setText(JSON.stringify(data));
+}).catch(error => setText(error));
 }
 
 export function chain() {
